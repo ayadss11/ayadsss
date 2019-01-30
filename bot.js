b3e5961e6ec68fce0a5d85ec5072866062a3be26
ayadss11 <46042075+ayadss11@users.noreply.github.com>
2019-01-30 15:40:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -132,6 +132,98 @@ if(message.content.startsWith(prefix + "bcall")) {
 
 
 
+client.on('message' , message => {
+  if (message.author.dark) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "ban") {
+               if(!message.channel.guild) return message.reply('** This command only for servers**');
+         
+  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
+  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
+  let user = message.mentions.users.first();
+  let reason = message.content.split(" ").slice(2).join(" ");
+      /*let banlog = client.channels.find("name", "ban-log");
+  if(!banlog) return message.reply("I've detected that this server doesn't have a ban-log text channel.");*/
+  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
+  if(!reason) return message.reply ("**اكتب سبب الباند**");
+  if (!message.guild.member(user)
+  .bannable) return message.reply("**لايمكنني ابند شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
+
+  message.guild.member(user).ban(7, user);
+  message.channel.sendMessage("**لقد تم اعطاء الباند الي شخص بنجاح** ✅");
+}
+});
+
+
+
+
+
+
+
+
+
+
+client.on("guildMemberAdd", (member) => {
+client.channels.get('506457510954401794').edit({name : `『 الأعضاء ↩ ${member.guild.memberCount} 』`});
+})
+client.on("guildMemberRemove", (member) => {
+client.channels.get('540148653134708747').edit({name : `『 الأعضاء ↩ ${member.guild.memberCount} 』`});
+})
+
+
+
+
+
+
+
+
+
+
+
+client.on("message", msg => {
+let ownerid = '441584713799303183'
+if(msg.content === `<@${ownerid}>`){
+bot.users.get(ownerid).send(`
+في واحد منشنك
+
+In server: :arrow_down:
+\`${msg.guild.name}\`
+ 
+By: :arrow_down:
+***${msg.author}***`);
+}
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
