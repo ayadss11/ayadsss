9634b4a4ffb90b6b8aac276922c076b5173da0a0
ayadss11 <46042075+ayadss11@users.noreply.github.com>
2019-01-30 20:30:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1073,7 +1073,7 @@ client.on('message', message => {
 
     client.on('message', message => {
               if (!message.channel.guild) return;
-      if(message.content =='^^members')
+      if(message.content =='!members')
       var IzRo = new Discord.RichEmbed()
       .setThumbnail(message.author.avatarURL)
       .setFooter(message.author.username, message.author.avatarURL)
@@ -1092,28 +1092,7 @@ client.on('message', message => {
 
 
          
-         
-        client.on('guildMemberAdd', member => {
 
-    const channel = member.guild.channels.find('name', '・text');
-  
-    const millis = new Date().getTime() - member.user.createdAt.getTime();
-    const now = new Date();
-    const createdAt = millis / 1000 / 60 / 60 / 24;
-
-
-
-
-  
-    const embed = new Discord.RichEmbed()
-    
-    .setColor("black")
-    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
-    .setAuthor(member.user.tag, member.user.avatarURL);
-    channel.sendEmbed(embed);
-
-  
-});
   
 
          
