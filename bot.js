655998a4ef3b16ed743b31899938c37068c78b81
ayadss11 <46042075+ayadss11@users.noreply.github.com>
2019-02-10 21:06:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,23 @@ console.log("log");
 
 
 
+client.on('ready', () => {
+    console.log('ready');
+});
+
+client.on('message', message => {
+    let args = message.content.split(' ').slice(1);
+
+    if(message.content.startsWith(prefix + 'dm')) {
+        let mnt = message.mentions.users.first();
+        if(!mnt) return message.reply('Please mention someone!');
+        mnt.send(args.join(' ').replace(mnt, '')).then(() => {
+            message.channel.send('Successfully sent the message!');
+        }).catch(() => {
+            message.channel.send('The user have dms disabled');
+        });
+    };
+})
 
 
 
