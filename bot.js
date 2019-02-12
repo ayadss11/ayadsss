5738ca49df4649dd5f42f8091ba15abdc088fa37
ayadss11 <46042075+ayadss11@users.noreply.github.com>
2019-02-12 19:56:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1509,7 +1509,7 @@ client.on('ready', () => {//new ready event
 client.on('message', message => {//new msg event
     if(!message.channel.guild) return;
       if(message.content.startsWith(prefix + 'rainbow')) {//to create the rainbow role
-       let rrole = message.guild.roles.find('name', 'VIP Rainbow')) return message.channel.send(`عندك الرتبة !`);
+       if(message.member.roles.find('name','VIP Rainbow')) return message.channel.send(`عندك الرتبة !`);
        message.member.addRole(rrole);
        var emdo = new Discord.RichEmbed()
        .setTitle(`:white_check_mark: **تم أعطائك الرتبة بنجاح!**`)
