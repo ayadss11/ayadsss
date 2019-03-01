2def3140d908565daae8f66606f344c717bd790a
ayadss11 <46042075+ayadss11@users.noreply.github.com>
2019-03-01 09:01:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -581,7 +581,7 @@ client.on('message', function(message) {
         .setThumbnail(`${message.author.avatarURL}`)
         .setDescription(`\n\n\`\`\`${message.content}\`\`\``)
         .setFooter(`From ${message.author.tag} (${message.author.presence.status.toUpperCase()})`)
-    client.channels.get("544213682242977810").send({embed:Dark});
+    client.channels.get("550920465955422249").send({embed:Dark});
     }
 });
 
@@ -772,43 +772,7 @@ if (message.content.toLowerCase().startsWith(prefix + 'banlist')){
 
 
 
-client.on('message', message => {
-            let args = message.content.split(' ').slice(1);
-            if(message.content.split(' ')[0] == `${prefix}color`){
-            const embedd = new Discord.RichEmbed()
-            .setFooter('Requested by '+message.author.username, message.author.avatarURL)
-            .setDescription(`**لا يوجد لون بهذا الأسم ** ❌ `)
-            .setColor(`ff0000`)
-           
-            if(!isNaN(args) && args.length > 0)
-           
-           
-            if    (!(message.guild.roles.find("name",`${args}`))) return  message.channel.sendEmbed(embedd);
-           
-           
-            var a = message.guild.roles.find("name",`${args}`)
-             if(!a)return;
-            const embed = new Discord.RichEmbed()
-           
-            .setFooter('Requested by '+message.author.username, message.author.avatarURL)
-            .setDescription(`**Done , تم تغير لونك . ✅ **`)
-           
-            .setColor(`${a.hexColor}`)
-            message.channel.sendEmbed(embed);
-            if (!args)return;
-            setInterval(function(){})
-               let count = 0;
-               let ecount = 0;
-            for(let x = 1; x < 201; x++){
-           
-            message.member.removeRole(message.guild.roles.find("name",`${x}`))
-           
-            }
-             message.member.addRole(message.guild.roles.find("name",`${args}`));
-           
-           
-            }
-            });
+
 
 
 
